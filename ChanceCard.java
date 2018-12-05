b680bc49cecb47f397b65c2ceafff4f26584174e
LoreleiLura <45364056+LoreleiLura@users.noreply.github.com>
2018-12-05 09:35:59 -0700

    Add files via upload

--- ChanceCard.java
+++ ChanceCard.java
@@ -1,5 +1,6 @@
 package edu.neumont.csc110;
 
 public class ChanceCard {
+	//Chance cards only hold a string
 	String[] chanceCards = new String[16];
 }
--- CommunityChestCard.java
+++ CommunityChestCard.java
@@ -1,5 +1,6 @@
 package edu.neumont.csc110;
 
 public class CommunityChestCard {
+	//community chest cards only hold strings
 	String[] communityChestCard = new String[17];
 }
