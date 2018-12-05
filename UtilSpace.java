0fe512d4d2f2bd888c2d866e9a4b9da0011e3d5a
LoreleiLura <45364056+LoreleiLura@users.noreply.github.com>
2018-12-05 09:21:48 -0700

    Add files via upload

--- UtilSpace.java
+++ UtilSpace.java
@@ -10,31 +10,40 @@ public class UtilSpace extends BoardSpace {
 
 	Random rand = new Random();
 
+	//init utilSpace
 	public UtilSpace(String Name, int Price) {
 		name = Name;
 		price = Price;
 	}
 
+	//init utilSpace dedicated class
 	public void init(String Name, int Price) {
 		name = Name;
 		price = Price;
 	}
-
+	
+	//set name of the space
 	public void setName(String Name) {
 		name = Name;
 	}
 
+	//set the price of the space
 	public void setPrice(int Price) {
 		price = Price;
 	}
+	
+	//return if the space is flagged as owned
 	public boolean getOwned() {
 		return isOwned;
 	}
+	
+	//set the owner. also flags as purchased when called
 	public void setOwner(Player i) {
 		owner = i;
 		isOwned=true;
 	}
 
+	//returns the rent value of the space
 	public int getRent(boolean otherOwned) {
 		int dice = (rand.nextInt(6)+1)+(rand.nextInt(6)+1);
 		if (otherOwned) {
@@ -44,6 +53,7 @@ public class UtilSpace extends BoardSpace {
 		}
 	}
 	
+	//returns a toString of the space.
 	public String toString() {
 		return name +" is owned by "+owner.getName()+".";
 	}
